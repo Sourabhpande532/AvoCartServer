36f1dbaa3a7e664a829e2410db0518ee96e01918
sourabhpande532 <sourabhpande43@gmail.com>
2026-07-14 15:05:16 +0530

    Fix-cores issue

--- index.js
+++ index.js
@@ -11,8 +11,20 @@ const passport = require("./config/passport");
 
 initializeDatabase();
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:3000",
+  "https://avo-cart-client.vercel.app"
+];
+
 const corsOption = {
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   optionsSuccessStatus: 200,
 };
